6d026010db2e3a9423ebf7c5b65f9b329c65beaa
AkshatJain9 <akshatt.jain9@gmail.com>
2024-09-02 16:12:13 +1000

    Basic Refactoring

--- main.py
+++ main.py
@@ -2,84 +2,120 @@ import os
 import flowkit as fk
 import numpy as np
 import matplotlib.pyplot as plt
+import platform
+import glob
 
 
-dataset = "Synthetic"
-
-data = "autoencoder_cytonorm"
-data = data + "/"
-
-panel_1 = data + "Panel1"
-panel_2 = data + "Panel2"
-panel_3 = data + "Panel3"
-
-print_mean = False
-print_cov = False
-print_emd = False
-plot_all_histograms_bool = True
-print_emd_2d = False
-
-# Utils
+################# GLOBAL VARIABLES #################
 scatter_channels = ['FSC-A', 'FSC-H', 'FSC-W', 'SSC-A', 'SSC-H', 'SSC-W']
 fluro_channels = ['BUV 395-A', 'BUV737-A', 'Pacific Blue-A', 'FITC-A', 'PerCP-Cy5-5-A', 'PE-A', 'PE-Cy7-A', 'APC-A', 'Alexa Fluor 700-A', 'APC-Cy7-A','BV510-A','BV605-A']
+new_channels = ['APC-Alexa 750 / APC-Cy7-A', 'Alexa 405 / Pac Blue-A', 'Qdot 605-A']
+fluro_channels += new_channels
+
 all_channels = scatter_channels + fluro_channels
+
 transform = fk.transforms.LogicleTransform('logicle', param_t=262144, param_w=0.5, param_m=4.5, param_a=0)
 
+dataset = "Synthetic"
+processing_type = "autoencoder_cytonorm"
+
+if (platform.system() == "Windows"):
+    somepath = ".\\" + dataset + "\\" + processing_type + "\\"
+else:
+    somepath = "./" + dataset + "/" + processing_type + "/"
+
+####################################################
+
+
+##################### UTILITY FUNCTIONS #####################
 def print_array(arr):
     np.set_printoptions(threshold=np.inf, linewidth=np.inf)
     print(np.array2string(arr, separator=', ', formatter={'float_kind':lambda x: f"{x:.2f}"}))
 
+def load_data(panel: str) -> np.ndarray:
+    if (platform.system() == "Windows"):
+        panel_root = somepath + panel + "\\"
+    else:
+        panel_root = somepath + panel + "/"
 
-# Data retrieval
-def get_np_array_from_sample(sample: fk.Sample, subsample: bool) -> np.ndarray:
-    sample_np = []
-    for ch in scatter_channels:
-        sample_np.append(sample.get_channel_events(sample.get_channel_index(ch), source='raw', subsample=subsample) / 262144)
-    
-    for ch in fluro_channels:
-        sample_np.append(sample.get_channel_events(sample.get_channel_index(ch), source='xform', subsample=subsample))
-    
-    sample_np = np.array(sample_np)
-    sample_np = np.transpose(sample_np)
-    return sample_np
-
-def get_events(panel):
     if (os.path.exists(panel + ".npy")):
-        return np.load(panel + ".npy")
-    all_samples = []
-    for root, _, files in os.walk(panel):
-        event_count = 0
-        for file in files:
-            file_path = "./" + panel + "/" + os.path.relpath(os.path.join(root, file), panel)
-            sample = fk.Sample(file_path)
-            sample.apply_compensation("./281122_Spillover_Matrix.csv")
-            sample.apply_transform(transform)
-            all_samples.append(get_np_array_from_sample(sample, False))
-            event_count += sample.event_count
-        print(f"Total events in {root}: {event_count}")
+        return np.load(panel_root + panel + ".npy")
 
-    res = np.vstack(all_samples)
-    np.save(panel + ".npy", res)
-    return res
+    # Recursively search for all .fcs files in the directory and subdirectories
+    fcs_files = glob.glob(os.path.join(panel_root, '**', '*.fcs'), recursive=True)
+    fcs_files_np = []
+
+    if (platform.system() == "Windows"):
+        spillover = "C:\\Users\\aksha\\Documents\\ANU\\COMP4550_(Honours)\\Spillovers\\281122_Spillover_Matrix.csv"
+    else:
+        spillover = "/home/akshat/Documents/281122_Spillover_Matrix.csv"
+    
+    # Load each .fcs file into fk.Sample and print it
+    for fcs_file in fcs_files:
+        sample = fk.Sample(fcs_file)
+        if "Panel" in panel:
+            sample.apply_compensation(spillover)
+        else:
+            sample.apply_compensation(sample.metadata['spill'])
+        sample.apply_transform(transform)
+        fcs_files_np.append(get_np_array_from_sample(sample, subsample=True))
+
+    stacked_np = np.vstack(fcs_files_np)
+    np.save(panel_root + panel + ".npy", stacked_np)
+    return stacked_np
+
+def get_channel_source(channel: str) -> str:
+    """ Get the source of the channel
+
+    Args:
+        channel: The channel to get the source of
+
+    Returns:
+        str: The source of the channel
+    """
+
+    if channel in scatter_channels:
+        return 'raw'
+    return 'xform'
+
+def get_factor(channel: str) -> float:
+    """ Get the factor to divide the channel by
+
+    Args:
+        channel: The channel to get the factor for
+
+    Returns:
+        float: The factor to divide the channel by
+    """
+
+    if channel in scatter_channels:
+        return 262144.0
+    return 1.0
+
+def get_np_array_from_sample(sample: fk.Sample, subsample: bool) -> np.ndarray:
+    """ Get a np.ndarray from a Sample object
+
+    Args:
+        sample: The Sample object to convert
+        subsample: Whether to subsample the data
+
+    Returns:
+        np.ndarray: The np.ndarray representation of the Sample object
+    """
+
+    return np.array([
+        sample.get_channel_events(sample.get_channel_index(ch), source=get_channel_source(ch), subsample=subsample) / get_factor(ch)
+        for ch in all_channels if ch in sample.pnn_labels
+    ]).T
+
+###################################################
 
 
-panel_1_np = get_events(panel_1)
-panel_2_np = get_events(panel_2)
-# panel_3_np = get_events(panel_3)
 
-# Mean Summary
+##################### BASIC SPREAD #####################
 def print_mean_summary(panel_np):
     mean_vector = np.mean(panel_np, axis=0)
     return mean_vector
-
-if print_mean:
-    p1_mean = print_mean_summary(panel_1_np)
-    p2_mean = print_mean_summary(panel_2_np)
-    p3_mean = print_mean_summary(panel_3_np)
-
-
-    print("Mean squared difference between panel 1 and panel 2: ", np.mean((p1_mean - p2_mean) ** 2))
-    print("Mean squared difference between panel 1 and panel 3: ", np.mean((p1_mean - p3_mean) ** 2))
 
 
 # Covariance Summary
@@ -87,17 +123,9 @@ def print_cov_summary(panel_np):
     cov_matrix = np.cov(panel_np, rowvar=False)
     return cov_matrix
 
-if print_cov:
-    p1_cov = print_cov_summary(panel_1_np)
-    p2_cov = print_cov_summary(panel_2_np)
-    p3_cov = print_cov_summary(panel_3_np)
-
-    print("Mean squared difference (covariance) between panel 1 and panel 2: ", np.mean((p1_cov - p2_cov) ** 2))
-    print("Mean squared difference (covariance) between panel 1 and panel 3: ", np.mean((p1_cov - p3_cov) ** 2))
 
 
-
-# Earth Mover's Distance
+###################### 1D EMD ######################
 def generate_histogram(panel_np, index):
     range = (0, 1)
 
@@ -117,44 +145,8 @@ def compute_all_emd(panel_np1, panel_np2):
         emds.append(compute_emd(hist1, hist2))
     return emds
 
-if print_emd:
-    emd_12 = compute_all_emd(panel_1_np, panel_2_np)
-    # emd_13 = compute_all_emd(panel_1_np, panel_3_np)
 
-    print("Earth Mover's Distance between panel 1 and panel 2: ", np.mean(emd_12))
-    # print("Earth Mover's Distance between panel 1 and panel 3: ", np.mean(emd_13))
-
-
-# Visualise the histograms
-def plot_all_histograms(panel_np_1, panel_np_2):
-    num_channels = len(all_channels)
-    
-    # Create a figure with subplots, one for each channel
-    fig, axs = plt.subplots(num_channels, 1, figsize=(8, 2*num_channels))
-    
-    # If there's only one subplot, axs won't be an array, so wrap it in one
-    if num_channels == 1:
-        axs = [axs]
-    
-    for i, ax in enumerate(axs):
-        hist1 = generate_histogram(panel_np_1, i)
-        hist2 = generate_histogram(panel_np_2, i)
-        
-        # Plot on the current axis
-        ax.plot(hist1, label='Panel 1')
-        ax.plot(hist2, label='Panel 2')
-        ax.legend()
-    
-    # Adjust layout to prevent overlapping
-    plt.tight_layout()
-    plt.show()
-
-if plot_all_histograms_bool:
-    plot_all_histograms(panel_1_np, panel_2_np)
-    plot_all_histograms(panel_1_np, panel_3_np)
-
-
-# 2D Histogram Stuff
+###################### 2D EMD ######################
 def gen_2d_histogram(panel_np, index1, index2):
     assert((index1 < 6 and index2 < 6) or (index1 >= 6 and index2 >= 6))
 
@@ -192,15 +184,31 @@ def compute_all_emd_2d(panel_np1, panel_np2):
 
     return emds_scatter, emds_fluro
 
-if print_emd_2d:
-    scatter_emd_12, fluro_emd_12 = compute_all_emd_2d(panel_1_np, panel_2_np)
-    scatter_emd_13, fluro_emd_13 = compute_all_emd_2d(panel_1_np, panel_3_np)
+###################### PLOTTING ######################
+def plot_all_histograms(panel_np_1, panel_np_2):
+    num_channels = len(all_channels)
+    
+    # Create a figure with subplots, one for each channel
+    fig, axs = plt.subplots(num_channels, 1, figsize=(8, 2*num_channels))
+    
+    # If there's only one subplot, axs won't be an array, so wrap it in one
+    if num_channels == 1:
+        axs = [axs]
+    
+    for i, ax in enumerate(axs):
+        hist1 = generate_histogram(panel_np_1, i)
+        hist2 = generate_histogram(panel_np_2, i)
+        
+        # Plot on the current axis
+        ax.plot(hist1, label='Panel 1')
+        ax.plot(hist2, label='Panel 2')
+        ax.legend()
+    
+    # Adjust layout to prevent overlapping
+    plt.tight_layout()
+    plt.show()
 
-    print("[2D] Earth Mover's Distance between panel 1 and panel 2 (scatter): ", np.mean(scatter_emd_12))
-    print("[2D] Earth Mover's Distance between panel 1 and panel 3 (scatter): ", np.mean(scatter_emd_13))
 
-    print("[2D] Earth Mover's Distance between panel 1 and panel 2 (fluro): ", np.mean(fluro_emd_12))
-    print("[2D] Earth Mover's Distance between panel 1 and panel 3 (fluro): ", np.mean(fluro_emd_13))
-
-
-
+if __name__ == "__main__":
+    b1 = "Panel1"
+    b2 = "Panel2"
