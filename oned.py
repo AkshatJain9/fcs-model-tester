71bc1745861d2b5da2756b8476b61d27c4c2f380
AkshatJain9 <akshatt.jain9@gmail.com>
2024-10-15 13:54:27 +1100

    Some plots

--- oned.py
+++ oned.py
@@ -32,8 +32,8 @@ def plot_fluoro_hist_compare(data_list, names=None, idx=None, synth_batch=True, 
     """
     num_datasets = len(data_list)
 
-    if num_datasets < 2:
-        print("Error: At least two datasets are required for comparison.")
+    if num_datasets < 1:
+        print("Error: At least one dataset are required for comparison.")
         return
 
     num_channels = data_list[0].shape[1]
@@ -124,10 +124,10 @@ if __name__ == "__main__":
 
     ########## ENU BATCHES ##########
     dataset = "ENU"
-    directory = "rawdata"
+    directory = "COMBAT_19635 _CD8"
     # batches = ["Plate 19635_CD8", "Plate 27902_N", "Plate 28332", "Plate 28528_N", "Plate 29178_N", "Plate 36841", "Plate 39630_N"]
     # batches = ["Plate 27902_N", "Plate 19635_CD8", "Plate 28332", "Plate 28528_N", "Plate 29178_N", "Plate 36841", "Plate 39630_N"]
-    batches = ["Plate 27902_N"]
+    batches = ["Plate 19635_CD8", "Plate 29178_N"]
     data_list = []
     names = []
     for batch in batches:
@@ -136,4 +136,4 @@ if __name__ == "__main__":
         data_list.append(data)
         names.append(batch)
 
-    plot_fluoro_hist_compare(data_list, names=names, synth_batch=False, file_name=f"{directory}_raw")
+    plot_fluoro_hist_compare(data_list, idx=6, names=names, synth_batch=False, file_name=f"{directory}")
